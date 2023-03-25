dd8bc55d597e58aea361c2f252ea893c58cad315
Joseph Lepley <josephflepley+pro@gmail.com>
2023-03-25 17:30:48 -0400

    Fix issue with React being bundled

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import external from 'rollup-plugin-peer-deps-external';
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
@@ -11,15 +12,18 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
+        exports: 'named',
         sourcemap: true,
       },
       {
         file: packageJson.module,
         format: 'esm',
+        exports: 'named',
         sourcemap: true,
       },
     ],
     plugins: [
+      external(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
